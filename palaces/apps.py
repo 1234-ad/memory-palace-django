b6283edbf4050ba5c6fc385c36db2fdecd1458a6
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-16 12:25:25 +0530

    Add app configuration

---
+++ palaces/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PalacesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'palaces'
+    verbose_name = 'Memory Palaces'
